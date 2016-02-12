91f3bd91b38cc4c8364dfbb3989dadfe55a25eda
rensso <soulchicano@gmail.com>
2016-02-12 17:07:01 -0200

    Update for standalone function HOM

--- Descriptor/Descriptor.cpp
+++ Descriptor/Descriptor.cpp
@@ -1,36 +1,28 @@
 // Descriptor.cpp : Defines the entry point for the console application.
 //
-
+#include "stdafx.h"
 
 #include "HOMDes.h"
 
 int _tmain(int argc, char ** argv)
 {
 	int cuboid_width	= 30,
-		cuboid_height	= 30;
-	//Optical flow class extractor, you may choose other
-	OpticalFlowOCV ofOCV; 
+		  cuboid_height	= 30;
+
+		
 	cv::Mat	img1 = cv::imread("043.tif"),
-			img2 = cv::imread("045.tif"),
-			img3 = cv::imread("047.tif");
-	OFdataType	in{ img1, img2, img3 };
-	OFvecParMat	outOf;
-	ofOCV.compute(in, outOf);
-	//__________________________________________________
-	DesInData	input;
+			    img2 = cv::imread("045.tif"),
+          img3 = cv::imread("047.tif");
+
+  std::vector<cv::Mat> images{ img1, img2, img3 };
+
 	std::vector<cutil_grig_point>	grid;
 	grid = grid_generator(img1.rows, img1.cols,
-		   cuboid_width, cuboid_height,
-		   cuboid_width, cuboid_height );
-	input.first		= outOf;
-	input.second	= grid;
-	DesOutData  output(grid.size());
-	//__________________________________________________
-	OFBasedDescriptorMO Descriptor( /*orientbin*/4, 
-									/*magnitudebin*/6, 
-									/*MaxMagnitude*/15, 
-									/*Magnitude thr*/0.1);
-	Descriptor.Describe(input, output);
+		                    cuboid_width, cuboid_height,
+		                    cuboid_width, cuboid_height );
+	  
+  DesOutData out;
+  HOM(out, images, grid, 4, 6, 15, 0.1);
 			
 	return 0;
 }
--- Descriptor/HOMDes.h
+++ Descriptor/HOMDes.h
@@ -13,9 +13,10 @@ typedef cuboid_dim cutil_grig_point;
 //ch = cuboid height
 //ov_w = overlap_width
 //ov_h = overlap_height
-std::vector<cutil_grig_point> grid_generator(int w, int h, int cw, int ch, int ov_w, int ov_h){
+std::vector<cutil_grig_point> grid_generator( int w, int h, int cw, int ch, 
+                                              int ov_w, int ov_h ){
 	int wfin = w - cw + 1,
-		hfin = h - ch + 1;
+		  hfin = h - ch + 1;
 	std::vector<cutil_grig_point> res;
 	for (int i = 0; i < wfin; i += ov_w){
 		for (int j = 0; j < hfin; j += ov_h){
@@ -31,25 +32,26 @@ std::ostream & operator << (std::ostream & os, cuboid_dim c){
 	os << "p2 " << c.xf << " " << c.yf;
 	return os;
 }
-
+////===========================================================================
+//main definitions 
 typedef std::pair< cv::Mat_<float>, cv::Mat_<float> >	DesparMat;
-typedef std::vector<DesparMat>							DesvecParMat;
-typedef std::vector<cutil_grig_point>					CuboTypeCont;
-typedef cv::Mat_<float>									HistoType;
-typedef std::pair<DesvecParMat, CuboTypeCont>			DesInData;	//input data type
-typedef std::vector<HistoType>							DesOutData;	//output data type
+typedef std::vector<DesparMat>                        DesvecParMat;
+typedef std::vector<cutil_grig_point>                 CuboTypeCont;
+typedef cv::Mat_<float>                               HistoType;
+typedef std::pair<DesvecParMat, CuboTypeCont>	        DesInData;	//input data 
+typedef std::vector<HistoType>                        DesOutData;	//output data
 
-//==================================================================
+//=============================================================================
 //descriptor magnitude orientation  
 struct OFBasedDescriptorMO
 {
 	int		_orientNumBin,
-			_magnitudeBin;
+        _magnitudeBin;
 	float	_maxMagnitude,
-			_thrMagnitude;
-	//______________________________________________________________
+			  _thrMagnitude;
+	//____________________________________________________________________________
 	
-	virtual void Describe(DesInData &in, DesOutData &out)
+	void Describe(DesInData &in, DesOutData &out)
 	{
 		double	binRange		= 360 / _orientNumBin,
 				binVelozRange	= _maxMagnitude / (float)_magnitudeBin;
@@ -60,7 +62,7 @@ struct OFBasedDescriptorMO
 			for (auto & imgPair : in.first){								// for each image
 				for (int i = cuboid.xi; i <= cuboid.xf; ++i){				//for each row 
 					for (int j = cuboid.yi; j <= cuboid.yf; ++j){			//for each pixel
-						if (imgPair.second(i, j) > _thrMagnitude){			//if magnitude is large enough
+						if (imgPair.second(i, j) > _thrMagnitude){			//large enough
 							int p = (int)(imgPair.first(i, j) / binRange);
 							int s = (int)(imgPair.second(i, j) / binVelozRange);
 							if (s >= _magnitudeBin) s = _magnitudeBin;
@@ -72,6 +74,7 @@ struct OFBasedDescriptorMO
 			out[cubPos++].push_back(histogram);		
 		}
 	}
+
 	OFBasedDescriptorMO(std::string file){
 		cv::FileStorage fs(file, cv::FileStorage::READ);
 		fs["descriptor_orientNumBin"] >> _orientNumBin;
@@ -87,16 +90,16 @@ struct OFBasedDescriptorMO
 	{}
 };
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//========================================================================================================================
-//main typedefs
-typedef std::vector< cv::Mat >							OFdataType;
+///////////////////////////////////////////////////////////////////////////////
+//=============================================================================
+//main typedefs for opticalflow 
+typedef std::vector< cv::Mat >							          OFdataType;
 typedef std::pair< cv::Mat_<float>, cv::Mat_<float> >	OFparMat;
-typedef std::vector<OFparMat>							OFvecParMat;
-//=========================================================================================================================
+typedef std::vector<OFparMat>							            OFvecParMat;
+//=============================================================================
 //heritance for Optical flow 
-struct OpticalFlowBase{	virtual void	compute(OFdataType &, OFvecParMat &) = 0;};
-//_________________________________________________________________________________________________________________________
+struct OpticalFlowBase{	virtual void compute(OFdataType &, OFvecParMat &)= 0;};
+//_____________________________________________________________________________
 
 struct OpticalFlowOCV : public OpticalFlowBase{
 	virtual void	compute(OFdataType & /*in*/, OFvecParMat & /*out*/);
@@ -121,21 +124,22 @@ static inline void FillPointsOriginal( std::vector<cv::Point2f> &vecPoints,
 	}
 }
 
-static inline void VecDesp2Mat( std::vector<cv::Point2f> &vecPoints, std::vector<cv::Point2f> &positions, 
+static inline void VecDesp2Mat( std::vector<cv::Point2f> &vecPoints, 
+                                std::vector<cv::Point2f> &positions, 
 								std::pair<cv::Mat_<float>, cv::Mat_<float> > & AMmat )
 {
 	float	magnitude,
-			angle,
-			catetoOpuesto,
-			catetoAdjacente;
-	//..................................................................................
+			  angle,
+			  catetoOpuesto,
+			  catetoAdjacente;
+	//...........................................................................
 	for (int i = 0; i < (int)positions.size(); ++i)	{
 		catetoOpuesto	= vecPoints[i].y - positions[i].y;
 		catetoAdjacente = vecPoints[i].x - positions[i].x;
 		//determining the magnite and the angle
 		magnitude = sqrt((catetoAdjacente	* catetoAdjacente) +
 						(catetoOpuesto		* catetoOpuesto));
-		//signed or unsigned---------------------------------------------------------!!!!
+		//signed or unsigned-------------------------------------------------------
 		angle = (float)atan2f(catetoOpuesto, catetoAdjacente) * 180 / CV_PI;
 		if (angle<0) angle += 360;
 
@@ -149,11 +153,12 @@ void OpticalFlowOCV::compute(OFdataType & in, OFvecParMat & out)
 	assert(in.size()>1);
 	std::vector<cv::Point2f> pointsprev, pointsnext, basePoints;
 	std::vector<uchar>		status;
-	cv::Mat					err;
-	cv::Size				winSize(31, 31);
-	cv::TermCriteria		termcrit(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 20, 0.3);
-	int						rows,
-							cols;
+	cv::Mat					      err;
+	cv::Size				      winSize(31, 31);
+	cv::TermCriteria		  termcrit( cv::TermCriteria::COUNT | 
+                                  cv::TermCriteria::EPS, 20, 0.3);
+	int	rows,
+			cols;
 	//.......................................................
 	rows = in[0].rows;
 	cols = in[0].cols;
@@ -180,6 +185,34 @@ void OpticalFlowOCV::compute(OFdataType & in, OFvecParMat & out)
 		out.push_back(data);
 	}
 	basePoints.clear();
+}
+
+//.............................................................................
+//main standalone function describe
+//This function receives a image set (cuboid- for instance 3 frames as temporal 
+//length), and receives the n rectangles that you want to describe, it is im_..
+//portant to note that in this example we are going to create a grid, but if...
+//you have especifc points, you can create a rectangle patch centered in the...
+//especific points. The function also receives a pointer tooptical flow base...
+//class. Opencv optical flow and frame substraction is set by default.
+//The function returns a vector with n histograms in outDes referenced variable 
+void HOM( 
+          DesOutData            &output,
+          std::vector<cv::Mat>  &images, 
+          std::vector<cutil_grig_point> &rects,
+          int   orientbin,
+          int   magnitudebin,
+          float maxMagnitude,
+          float thrMagnitude,
+          OpticalFlowBase * of = new OpticalFlowOCV ){
+  OFvecParMat outOf;
+  of->compute(images, outOf);
+  DesInData   input ( outOf, rects );
+  output.clear();
+  output.resize( rects.size() );
+  OFBasedDescriptorMO Descriptor( orientbin, magnitudebin, maxMagnitude,
+                                  thrMagnitude);
+  Descriptor.Describe(input, output);
 }
 
 #endif
